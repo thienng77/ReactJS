f9e59987060384eb5f54b9c8c36589a98951d7ce
LENOVO\Gia Thien <ngogiathien007@gmail.com>
2025-08-01 15:38:40 +0700

    video 17

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -2,11 +2,18 @@
 // function component
 import React from "react";
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'Thien',
+        address: 'Binh Dinh',
+        age: 26
+    };
+
     //JSX
     render() {
         return (
-            <div> my first component 
-                {Math.random()}
+            <div>
+                My name is {this.state.name}
             </div>
         );
     }

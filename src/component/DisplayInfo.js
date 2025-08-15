c154d3219a6b87edfba18b8fda46540f3a8451d8
LENOVO\Gia Thien <ngogiathien007@gmail.com>
2025-08-15 08:52:12 +0700

    Outputting List

--- src/component/DisplayInfo.js
+++ src/component/DisplayInfo.js
@@ -1,16 +1,34 @@
 import react from "react";
 import React from "react";
 
-class DisplayInfo extends React.Component{
-    render(){
+class DisplayInfo extends React.Component {
+    render() {
         //console.log(this.props)
         //destructuring array/object
-        const {age, name} = this.props;
+        const { listUsers } = this.props;
+        console.log(listUsers);
         //props -> viết tắt property
-        return(
+        return (
             <div>
+                {
+                    listUsers.map((user) => {
+                        return (
+                            <div key={user.id}>
+                                <div>My name's {user.name}</div>
+                                <div>My age's {user.age}</div>
+                                <hr/>
+                            </div>
+                        )
+                    })
+                }
+                {/* <div>My name's {name}</div>
+                <div>My age's {age}</div>
+                <hr/>
                 <div>My name's {name}</div>
                 <div>My age's {age}</div>
+                <hr/>
+                <div>My name's {name}</div>
+                <div>My age's {age}</div> */}
             </div>
         )
     }
--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -5,15 +5,21 @@ import UserInfo from "./UserInfo";
 import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component {
     //JSX
+    state = {
+        listUsers: [
+            { id: 1, name: "Ngo Gia Thiên", age: "26" },
+            { id: 2, name: "Ngo Quốc Thiên", age: "27" },
+            { id: 3, name: "Ngo Hoàng Thiên", age: "28" },
+        ]
+    }
     render() {
-        const myInfo = ['ab','b','c'];
+        //DRY: don't repeat yourseft
         return (
             <div>
                 <UserInfo />
-                <br/><br/>
-                <DisplayInfo name="Thien IT" age="26"/>
-                <hr/>
-                <DisplayInfo name={"Gia Thiên"} age={26} myInfo={myInfo}/>
+                <br /><br />
+                <DisplayInfo listUsers={this.state.listUsers}
+                />
             </div>
         );
     }

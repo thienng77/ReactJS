864195d65439efe0527c7815200d07c403ded7d2
LENOVO\Gia Thien <ngogiathien007@gmail.com>
2025-10-08 10:03:05 +0700

    css với react

--- src/component/DisplayInfo.js
+++ src/component/DisplayInfo.js
@@ -1,6 +1,6 @@
 import react from "react";
 import React from "react";
-
+import './DisplayInfo.scss'
 class DisplayInfo extends React.Component {
     state = {
         isShowListUser : true
@@ -11,13 +11,9 @@ class DisplayInfo extends React.Component {
        })
     }
     render() {
-        //console.log(this.props)
-        //destructuring array/object
         const { listUsers } = this.props;
-        console.log(listUsers);
-        //props -> viết tắt property
         return (
-            <div>
+            <div className="display-info-container">
                 <div>
                     <span onClick={() => { this.handleShowHIde() }}>{this.state.isShowListUser === true ? "Hide list users:" : "Show list users:"} </span>
                 </div>
@@ -26,7 +22,7 @@ class DisplayInfo extends React.Component {
                         {
                             listUsers.map((user) => {
                                 return (
-                                    <div key={user.id} className={user.age > 18 ? "green" : "blue"}>
+                                    <div key={user.id} className={user.age > 18 ? "red" : "blue"}>
                                         <div>My name's {user.name}</div>
                                         <div>My age's {user.age}</div>
                                         <hr />
